ccecaeeea384eb5afe1318519ccaa84b2304e3ce
ricardodsb <ricardojdasilva15@gmail.com>
2021-10-14 19:52:10 +0000

    almost done

--- src/js/component/cards.jsx
+++ src/js/component/cards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "./body.jsx";
+import { Card } from "./card.jsx";
 
 export const Cards = () => {
 	return (
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export const NavBar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-light sticky top">
-			<a className="navbar-brand mr-auto" href="#">
+		<nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top justify-content-between">
+			<a className="navbar-brand" href="#">
 				Start Bootstrap
 			</a>
 			<button
@@ -17,7 +17,7 @@ export const NavBar = () => {
 				<span className="navbar-toggler-icon"></span>
 			</button>
 			<div className="collapse navbar-collapse" id="navbarNav">
-				<ul className="navbar-nav ml-auto ">
+				<ul className="navbar-nav">
 					<li className="nav-item active">
 						<a className="nav-link" href="#">
 							Home <span className="sr-only">(current)</span>
